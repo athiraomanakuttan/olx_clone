3a22aab08abe93d656b4b7d5596968114e6e8075
athira <athiraomanakuttan20@gmail.com>
2024-10-01 17:02:32 +0530

    Working with products

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,48 @@
-import Navbar from '../../component/Navbar/Navbar'
-import './Home.css'
+import { useEffect, useState } from 'react';
+import Menubar from '../../component/Menubar/Menubar';
+import Navbar from '../../component/Navbar/Navbar';
+import './Home.css';
+import Product from '../../component/Product/Product';
+
+type ProductType = {
+  id: number;
+  title: string;
+  price: string;
+  category: string;
+  description: string;
+  image: string;
+};
+
+const allProducts = async (): Promise<ProductType[]> => {
+  const response = await fetch('https://fakestoreapi.com/products');
+  const data = await response.json();
+  return data;
+};
 
 const Home = () => {
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const productData = await allProducts();
+        console.log("ProductData", productData)
+        setProducts(productData);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
-     <Navbar />
+      <Navbar />
+      <Menubar />
+      <Product productData={products} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/component/Menubar/Menubar.tsx
@@ -0,0 +1,20 @@
+import './Menubar.css'
+const Menubar = () => {
+  return (
+    <div>
+      <div className="menubar">
+        <ul className='flex items-center gap-5 justify-center p-3'>
+            <li>Car</li>
+            <li>Motorcycle</li>
+            <li>Mobile phone</li>
+            <li>House</li>
+            <li>Scooter</li>
+            <li>Bike</li>
+            <li>Apartments</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Menubar
